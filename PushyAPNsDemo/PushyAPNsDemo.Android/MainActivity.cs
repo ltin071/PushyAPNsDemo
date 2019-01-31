36f6ee2a3edf08994e3de11a4bea18aa53d4d257
Samuel Leung <tinching.leung@gmail.com>
2019-01-31 12:15:13 +0800

    Android Pushy working

--- PushyAPNsDemo/PushyAPNsDemo.Android/MainActivity.cs
+++ PushyAPNsDemo/PushyAPNsDemo.Android/MainActivity.cs
@@ -6,6 +6,12 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using ME.Pushy.Sdk;
+using System.Threading.Tasks;
+using Android.Util;
+using Android;
+using Android.Support.V4.Content;
+using Android.Support.V4.App;
 
 namespace PushyAPNsDemo.Droid
 {
@@ -18,8 +24,52 @@ namespace PushyAPNsDemo.Droid
             ToolbarResource = Resource.Layout.Toolbar;
 
             base.OnCreate(savedInstanceState);
+            Pushy.Listen(this);
+            // Check whether the user has granted us the READ/WRITE_EXTERNAL_STORAGE permissions
+            if (ContextCompat.CheckSelfPermission(Application.Context, Manifest.Permission.WriteExternalStorage) != Permission.Granted)
+            {
+                // Request both READ_EXTERNAL_STORAGE and WRITE_EXTERNAL_STORAGE so that the
+                // Pushy SDK will be able to persist the device token in the external storage
+                ActivityCompat.RequestPermissions(this, new string[] { Manifest.Permission.ReadExternalStorage, Manifest.Permission.WriteExternalStorage }, 0);
+            }
+
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+
             LoadApplication(new App());
+            RegPushy();
         }
+        private async void RegPushy()
+        {
+                await RegisterForPushNotifications();
+            
+        }
+        private async Task RegisterForPushNotifications()
+        {
+            // Execute Pushy.Register() in a background thread
+            await Task.Run(() =>
+            {
+                try
+                {
+                    // Assign a unique token to this device
+                    string deviceToken = Pushy.Register(Application.Context);
+                    Pushy.Subscribe("news", Application.Context);
+
+                    // Log it for debugging purposes
+                    Log.Debug("MyApp", "Pushy device token: " + deviceToken);
+
+                    // Send the token to your backend server via an HTTP GET request
+                    //new URL("https://{YOUR_API_HOSTNAME}/register/device?token=" + deviceToken).OpenConnection();
+                }
+                catch (Exception exc)
+                {
+                    // Log error to console
+                    Log.Error("MyApp", exc.Message, exc);
+                    return;
+                }
+
+                // Succeeded, optionally do something to alert the user
+            });
+        }
+
     }
 }
